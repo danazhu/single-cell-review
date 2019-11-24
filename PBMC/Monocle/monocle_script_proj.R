9badc40bfc2ab2b00c93053e998ae9cfaa4dd7ba
Dana Zhu <danazhu@kickseed.dcmb.ybinternal>
2019-11-24 17:44:20 -0500

    monocle script now stores ari values

--- PBMC/Monocle/monocle_script_proj.R
+++ PBMC/Monocle/monocle_script_proj.R
@@ -5,22 +5,35 @@ library(monocle)
 tt = c()
 
 DATASETS <- c("100", "1k")
+TRIALS <- c(1:5)
+
+# in case ari and time output file exists already
+file.remove("monocle_ari_time_output.txt")
 
 for (ds in DATASETS) {
 	dataset <- read.table(paste0('A_', ds, '.txt'))
-
 	dataset <- as.matrix(dataset)
 
-	start_time <- Sys.time()
+	write.table(paste0("ari values for ", ds, " sample size"), "monocle_ari_time_output.txt", append = TRUE, sep = "\n", col.names = FALSE, row.names = FALSE)
 
-	data <- newCellDataSet(dataset)
-	data <- estimateSizeFactors(data)
-	data <- reduceDimension(data, max_components = 3, num_dim = 10, reduction_method = 'tSNE', verbose = T, check_duplicates = F)
-	data <- clusterCells(data, num_clusters = 10)
+	for (num in TRIALS) {
+		start_time <- Sys.time()
 
-	end_time <- Sys.time()
-	t = end_time - start_time
-	print(t)
+		data <- newCellDataSet(dataset)
+		data <- estimateSizeFactors(data)
+		data <- reduceDimension(data, max_components = 3, num_dim = 10, reduction_method = 'tSNE', verbose = T, check_duplicates = F)
+		data <- clusterCells(data, num_clusters = 10)
 
-	write.csv(data$Cluster, sprintf(paste0("monocle_output_", ds, ".txt")));
+		end_time <- Sys.time()
+		t = end_time - start_time
+		#print(t)
+		print(end_time - start_time)
+		
+		# TODO: calculate ari values
+
+		# record time spent
+		write.table(end_time - start_time, "monocle_ari_time_output.txt", append = TRUE, sep="\n", col.names = FALSE, row.names = FALSE)
+
+		write.csv(data$Cluster, sprintf(paste0("monocle_output_", ds, "_", num, ".txt")));
+	}
 }
