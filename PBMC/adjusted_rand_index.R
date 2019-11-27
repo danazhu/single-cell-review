de515256f9cd1831ff4e4c2b18214753c9beaec8
Catherine Ziyu Liu <liuzcat@kickseed.dcmb.ybinternal>
2019-11-26 19:51:06 -0500

    Move adjusted_rand_index to the correct spot

--- PBMC/adjusted_rand_index.R
+++ PBMC/adjusted_rand_index.R
@@ -8,13 +8,27 @@ p1 <- read.delim("single-cell-review/PBMC/Monocle/monocle_output_100_1.txt", hea
 p1 <- p1$x
 p1 <- matrix(p1, nrow = length(p1), ncol = 1)
 
-RI_100 <- compute_ARI(p1, y_100)
+RI_monocle_100 <- compute_ARI(p1, y_100)
+
+# Sample: Test pcaReduce 100 result
+pca1 <- read.delim("single-cell-review/PBMC/pcaReduce/pca_output_100_1.txt", header = TRUE, sep = ",")
+pca1 <- pca1$Cl_mat
+pca1 <- matrix(pca1, nrow = length(pca1), ncol = 1)
+
+RI_pca_100 <- compute_ARI(pca1, y_100)
 
 # Sample: Test Monocle 1k result
 p2 <- read.delim("single-cell-review/PBMC/Monocle/monocle_output_1k_4.txt", header = TRUE, sep = ",")
 p2 <- p2$x
 p2 <- matrix(p2, nrow = length(p2), ncol = 1)
-RI_1k <- compute_ARI(p2, y_1k)
+RI_monocle_1k <- compute_ARI(p2, y_1k)
+
+# Sample: Test pcaReduce 1k result
+pca2 <- read.delim("single-cell-review/PBMC/pcaReduce/pca_output_1k_1.txt", header = TRUE, sep = ",")
+pca2 <- pca2$Cl_mat
+pca2 <- matrix(pca2, nrow = length(pca2), ncol = 1)
+
+RI_pca_1k <- compute_ARI(pca2, y_1k)
 
 # Manually compute adjusted RI
 compute_ARI <- function(p1, p2) {
